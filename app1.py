f02e3bf755030ddff361b1baa52598492d827c58
Devlatino <129116972+Devlatino@users.noreply.github.com>
2025-04-03 19:19:16 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 from selenium import webdriver
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
