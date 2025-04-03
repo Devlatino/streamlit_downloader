b3d2ea134461d63f11b21d29f36562f8726cbce7
Devlatino <129116972+Devlatino@users.noreply.github.com>
2025-04-03 19:16:41 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,8 +1,9 @@
 import streamlit as st
-import os  # Add this line
+import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 # Configura la directory di download
 
 download_dir = os.path.join(os.getcwd(), "downloads")  # Relative to app’s working directory
